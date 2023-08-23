8166a22a8bc5a3f9c51042548a0ab46b9ba6d648
Alejo Castaño <drskantus@hotmail.com>
2023-08-23 23:15:51 +0200

    Initialise Expo project

---
+++ App.tsx
@@ -0,0 +1,6 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/components/Home/home';
+
+export default function App() {
+  return <Home />
+}
---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+const BASE_URL = 'https://pokeapi.co/api/v2/';
+const POKEMON_URL = `${BASE_URL}pokemon/`;
+
+
+export { BASE_URL, POKEMON_URL  };
